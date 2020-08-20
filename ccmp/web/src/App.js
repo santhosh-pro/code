e5a7d3bf66333f19ccfd59e5e13ac0a406a636b0
Ivano Carvalho <ivanoca.dev@gmail.com>
2020-08-20 17:28:20 -0300

    desenv

---
+++ ccmp/web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Customer from './components/customer';
+
+import './assets/styles/global.scss';
+
+function App() {
+
+  return (
+
+    <Customer />
+   
+  );
+
+}
+
+export default App;
---
+++ ccmp/web/src/components/customer/index.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class Customer extends Component {
+
+    constructor () {
+
+        super();
+
+        this.state = {
+            name : ''
+        }
+
+    }
+
+    hangleChange = ( e ) => {
+
+        this.setState({
+            name : e.target.value
+        });
+
+    }
+
+
+    componentWillMount() {
+        console.log('componentWillMount');
+        
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentWillUpdate() {
+        console.log('componentWillUpdate');
+    }
+    
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+
+
+    render () {
+
+        return (
+
+            <div action="" id="page-customer">
+                <div id="page-customer-content" className="container" >
+                    
+                    <header className="page-header">
+                        <span>Customer</span>
+                    </header>
+                
+                    <div type="text" className="input-block">
+                        <label>                            
+                            Nome
+                        </label>
+                        <input 
+                            type="text" 
+                            className="input-customer-name"
+                             onChange={(e)=>this.hangleChange(e)} 
+                             value={this.state.name} 
+                        />
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default Customer;
+
